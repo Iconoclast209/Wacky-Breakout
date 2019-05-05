b32887c20adcf4a927fb6da4acfd6daa15a40078
asmith <mrpresidentsmith@gmail.com>
2019-05-05 11:10:54 -0700

    Adjusted paddle implementation.  Added a ball and started ball implementation.

--- Wacky Breakout/Assets/Scripts/Paddle.cs
+++ Wacky Breakout/Assets/Scripts/Paddle.cs
@@ -8,43 +8,29 @@ public class Paddle : MonoBehaviour
     [SerializeField]
     float speed = 0.4f;
     [SerializeField]
-    float screenEdgeBuffer = 0.2f;
+    float screenEdgeBuffer = 0.1f;
 
     Rigidbody2D rb2d;
     float halfColliderWidth;
     #endregion
 
     #region METHODS
-    // Start is called before the first frame update
+    
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        halfColliderWidth = GetComponent<BoxCollider2D>().size.x / 2 + screenEdgeBuffer;
+        halfColliderWidth = GetComponent<BoxCollider2D>().size.x / 2;
     }
-
 
     void FixedUpdate()
     {
-        if(Input.GetAxis("Horizontal") > 0)
+        if(Input.GetAxis("Horizontal") != 0)
         {
-            //Move Right
             Vector2 destination = new Vector2(transform.position.x, transform.position.y);
             destination += Vector2.right * speed * Input.GetAxis("Horizontal");
-            if(destination.x + halfColliderWidth < ScreenUtils.ScreenRight)
-            {
-                rb2d.MovePosition(destination);
-            }
-                        
-        }
-        else if(Input.GetAxis("Horizontal") < 0)
-        {
-            //Move Left
-            Vector2 destination = new Vector2(transform.position.x, transform.position.y);
-            destination -= Vector2.left * speed * Input.GetAxis("Horizontal");
-            if (destination.x - halfColliderWidth > ScreenUtils.ScreenLeft)
-            {
-                rb2d.MovePosition(destination);
-            }
+            destination.x = Mathf.Clamp(destination.x, ScreenUtils.ScreenLeft + halfColliderWidth + screenEdgeBuffer,
+                ScreenUtils.ScreenRight - halfColliderWidth - screenEdgeBuffer);
+            rb2d.MovePosition(destination);
         }
     }
     #endregion
