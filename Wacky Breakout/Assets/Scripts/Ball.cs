dde11c5c3e724104768b8bb53e3cea35aa78f3b8
asmith <mrpresidentsmith@gmail.com>
2019-05-09 05:43:22 -0700

    Implemented ball destruction after a given time period.

--- Wacky Breakout/Assets/Scripts/Ball.cs
+++ Wacky Breakout/Assets/Scripts/Ball.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [SerializeField]
+    float lifetime = 5f;
+    float elapsedTime = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Tell GameController a new ball exists
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        elapsedTime += Time.deltaTime;
+        if(elapsedTime > lifetime)
+        {
+            DestroyMe();
+        }
+    }
+
+    void DestroyMe()
+    {
+        //Tell GameController this ball is gone
+        Destroy(this.gameObject);
     }
 }
