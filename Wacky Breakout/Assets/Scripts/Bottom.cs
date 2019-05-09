1b8f1e93a4f203e84b5b92916fe9f356b27599bb
asmith <mrpresidentsmith@gmail.com>
2019-05-09 16:37:04 -0700

    Implemented bottom trigger collider to record when a ball leaves the bottom of the play area.  Implemented the GameController with functions to keep track of score and remaining balls.

---
+++ Wacky Breakout/Assets/Scripts/Bottom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottom : MonoBehaviour
+{
+    GameController gameController;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ball"))
+        {
+            gameController.RecordLossOfBall();
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Wacky Breakout/Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    [SerializeField]
+    int score = 0;
+    [SerializeField]
+    Text scoreText;
+    [SerializeField]
+    Text ballsLeftText;
+
+    int ballsRemaining = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score:  " + score;
+        ballsLeftText.text = "Balls Left:  " + ballsRemaining;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RecordScore(int a)
+    {
+        score += a;
+        scoreText.text = "Score:  " + score;
+    }
+
+    public void RecordLossOfBall()
+    {
+        ballsRemaining--;
+        ballsLeftText.text = "Balls Left:  " + ballsRemaining;
+    }
+}
